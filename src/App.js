e50334bd8b57e49f4970673c134e3f615a0d6578
Jeff Korzeniowski <Jeff@gratuitymedia.com>
2022-10-12 12:05:58 -0400

    Added CSS styles and started sanitizing user inputs.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [bill, setBill] = useState("");
+  const [tip, setTip] = useState("10%");
+  const [split, setSplit] = useState(1);
+  function handleTipChange(e) {
+    let value = e.target.value.replace("%", "");
+    if (value.indexOf("%") === -1) {
+      value = value + "%";
+    }
+    setTip(value);
+  }
+  function splitMinus() {
+    setSplit((oldValue) => {
+      if (oldValue === 1) {
+        return 1;
+      } else {
+        return oldValue - 1;
+      }
+    });
+  }
+  function splitPlus() {
+    setSplit((oldValue) => oldValue + 1);
+  }
   return (
     <div>
-        <label></label>
-        <input type="text" placeholder={'0.00'}/>
+      <label>Bill total</label>
+      <input
+        type="text"
+        placeholder={"0.00"}
+        value={bill}
+        onChange={(e) => setBill(e.target.value)}
+      />
+      <label>Tip</label>
+      <input
+        type="text"
+        placeholder={"0.00"}
+        value={tip}
+        onChange={(e) => setTip(e.target.value)}
+      />
+      <div className="summary">
+        <div className="split">
+          <label>Split</label>
+          <div className="split-control">
+            <button onClick={splitMinus}>-</button>
+            <span>{split}</span>
+            <button onClick={splitPlus}>+</button>
+          </div>
+        </div>
+        <div className="result">
+          <label>Split total</label>
+          <span>175.00</span>
+        </div>
+      </div>
     </div>
   );
 }
